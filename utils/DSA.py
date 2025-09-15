acf3bedf15d3f9a4c80dd8c87192d21ebf27d018
lankabelgezogen <thomas.woelkhart@tum.de>
2025-09-15 11:00:54 -0400

    feat: use NaturalImage2DIO for PNG loading for DSA

--- utils/DSA.py
+++ utils/DSA.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn.functional as F
 from PIL import Image
 from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
+from nnunetv2.imageio.natural_image_reader_writer import NaturalImage2DIO
 import os
 import pydicom
 
@@ -10,32 +11,30 @@ import pydicom
 def preprocess_image(input_path):
     ext = os.path.splitext(input_path)[1].lower()
     if ext == ".png":
-        img = Image.open(input_path)
-        img = img.convert("L")
-        img = np.array(img)
-        return img
+        imgio = NaturalImage2DIO()
+        img, props = imgio.read_images((input_path,))
+        return img[0]
     elif ext == ".dcm":
         ds = pydicom.dcmread(input_path)
         img = ds.pixel_array
-        minip = np.min(img, axis=0)
-        return minip
+        return np.min(img, axis=0)
     else:
         raise ValueError(f"Unsupported file type: {ext}")
 
 
 def run_inference_DSA(
-    image_np: np.ndarray | str,
+    image: str,
     target_size: tuple[int, int] = (512, 512),
     predictor: nnUNetPredictor = None,
 ) -> np.ndarray:
-    if isinstance(image_np, str):
-        image_np = preprocess_image(image_np)
+    if isinstance(image, str):
+        image = preprocess_image(image)
 
-    if image_np.ndim != 2:
-        raise ValueError(f"Expected 2D grayscale image, got shape {image_np.shape}")
+    if image.ndim != 2:
+        raise ValueError(f"Expected 2D grayscale image, got shape {image.shape}")
 
     img_torch = (
-        torch.from_numpy(image_np.astype(np.float32)).unsqueeze(0).unsqueeze(0)
+        torch.from_numpy(image.astype(np.float32)).unsqueeze(0).unsqueeze(0)
     )  # (1, 1, H, W)
 
     # Z-score normalization (per image)
